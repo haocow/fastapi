fbfaea9583bd71ab67d95215f68cb3dd5366a138
haocow <jhao19@gmail.com>
2022-09-22 16:36:54 -0400

    add CORS middleware and dummy register endpoint

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "https://fastapi-production-e81d.up.railway.app"
+    "https://site-production-1870.up.railway.app",
+    "https://jonhao.com",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Msg(BaseModel):
     msg: str
@@ -25,3 +41,13 @@ async def demo_post(inp: Msg):
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+class Data(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+
+@app.post("/auth/register")
+async def register_user(data: Data):
+    return {"message": f"Registered user: {data.email}"}
